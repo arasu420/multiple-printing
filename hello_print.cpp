33f926c3323559c60aa4abd927deeb6cd7aefafe
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-02-20 16:54:02 +0530

    Add files via upload

---
+++ hello_print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cout << "Enter the no. of times to print: ";
+cin >> n;
+for(int i=0;i<n;i++)
+{
+    cout << "HELLO" << endl;
+}
+return 0;
+}
